09d6e53b3a28ee81577991cd9d429a59cd029be4
Altinsk <119261430+Altinsk@users.noreply.github.com>
2023-12-04 06:13:34 +0000

    adjust call api

--- sportify-frontend/src/components/Dashboard/Dashboard.jsx
+++ sportify-frontend/src/components/Dashboard/Dashboard.jsx
@@ -29,7 +29,6 @@ export default function Dashboard() {
     fetchActivity();
     fetchAverageSessions();
     fetchUserPerformance();
-
     fetchUserData();
 
     async function fetchUserData() {
