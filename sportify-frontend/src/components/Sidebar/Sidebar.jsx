9fcc36af9ffc20247ba8e2c25bdfa5397b6dd05a
Altinsk <119261430+Altinsk@users.noreply.github.com>
2023-12-10 09:23:49 +0000

    modified yoga icon

--- sportify-frontend/src/components/Sidebar/Sidebar.jsx
+++ sportify-frontend/src/components/Sidebar/Sidebar.jsx
@@ -4,13 +4,14 @@ import IconYoga from "../../icons/IconYoga";
 import Icon1 from "../../icons/png-Icons/icon (1).png";
 import Icon2 from "../../icons/png-Icons/icon (2).png";
 import Icon3 from "../../icons/png-Icons/icon (3).png";
+import yogaPic from "../../icons/png-Icons/iconyoga.png";
 
 const Drawer = () => {
   return (
     <section className="sidebar">
       <article className="icons">
         <div className="side-icons side-icon--extra">
-          <IconYoga />
+          <img src={yogaPic} alt="Icon1" />
         </div>
         <div className="side-icons">
           <img src={Icon1} alt="Icon1" />
